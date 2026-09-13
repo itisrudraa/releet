a1f4ec071948808732c4c69998cc448663d577c0
Rudra <merudra01@gmail.com>
2026-09-13 13:49:11 +0530

    chore: configure Tailwind Vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { crx } from "@crxjs/vite-plugin";
+import tailwindcss from "@tailwindcss/vite";
 import manifest from "./src/manifest";
 
 export default defineConfig({
   plugins: [
     react(),
-    crx({ manifest })
-  ]
+    tailwindcss(),
+    crx({ manifest }),
+  ],
 });
